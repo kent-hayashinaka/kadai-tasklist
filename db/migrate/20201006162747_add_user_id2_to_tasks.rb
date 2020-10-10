6389b48f50c40b405a42b25d131a3e8698156129
kento.hayashinaka <clark0821kent@gmail.com>
2020-10-10 16:07:25 +0000

    heroku tasklist

--- db/migrate/20201006162747_add_user_id2_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddUserId2ToTasks < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :tasks, :user, foreign_key: true
-  end
-end
--- db/migrate/20201010160147_remove_user_id_id_from_tasks.rb
+++ db/migrate/20201010160147_remove_user_id_id_from_tasks.rb
@@ -1,5 +1,5 @@
 class RemoveUserIdIdFromTasks < ActiveRecord::Migration[5.2]
   def change
-    remove_column :tasks, :user_id, foreign_key: true
+    remove_column :tasks, :user_id_id, :integer
   end
 end
