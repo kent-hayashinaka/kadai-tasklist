279e0907a9da62d62179a25a71929544326e0f1e
kento.hayashinaka <clark0821kent@gmail.com>
2020-10-16 13:47:44 +0000

    login_url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:show]
-  
-  def show
-     @user = User.find(params[:id])
-  end
+
 
   def new
      @user = User.new
@@ -14,7 +10,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to login_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
