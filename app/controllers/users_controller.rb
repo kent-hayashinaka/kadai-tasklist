65715c625f1d986db36bce488cf953812f14de8f
kento.hayashinaka <clark0821kent@gmail.com>
2020-10-16 13:21:17 +0000

    add show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+  
+  def show
+     @user = User.find(params[:id])
+  end
 
   def new
      @user = User.new
